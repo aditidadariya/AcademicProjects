bc8d1823a5303cffeed95b92ae76d89271737e47
Aditi Dadariya <103076396+aditidadariya@users.noreply.github.com>
2022-09-29 15:35:13 +0100

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 12:54:20 2022
+
+@author: aditidadariya
+"""
+
+"""  Config.py  """
+
+"""############################################ Variable Declaration ############################################"""
+
+# location_of_airportfile variable is defined to store the airport dataset file location
+location_of_airportfile = '/Users/aditidadariya/Aditi Personal/UK Universities/University of Reading/Modules/CSMBD21_Big Data and Cloud Computing/Asssessment/Cloud Computing data/Top30_airports_LatLong.csv'
+
+# location_of_passengerfile variable is defined to store the passenger dataset file location
+location_of_passengerfile = '/Users/aditidadariya/Aditi Personal/UK Universities/University of Reading/Modules/CSMBD21_Big Data and Cloud Computing/Asssessment/Cloud Computing data/AComp_Passenger_data_no_error.csv'
+
+# datasetairport variable is defined to store the list of airport dataset
+datasetairport = []
+
+# datasetpassenger variable is defined to store the list of passenger dataset
+datasetpassenger = []
+
+# datasetupdatedpassenger variable is defined to store the list of updated passenger dataset
+datasetupdatedpassenger = []
+
+# flightairportlist variable is defined to store the list of NewMapper1.py mapper output
+flightairportlist = []
+
+# flightairportreduced variable is defined to store the result list in NewReducer function
+flightairportreduced = []
+
+# passengerflightlist variable is defined to store the list of NewMapper1.py mapper output
+passengerflightlist = []
+
+# passengerflightreduced variable is defined to store the result list in NewReducer2 function
+passengerflightreduced = []
+
+# passengerflighttotal variable id defined to store the total number of flights of all passengers
+passengerflighttotal = []
+
+# sortlist variable defined to store the sorted list
+sortlist = []
+
+
+
